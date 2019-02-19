ad9b9e0354c222e1c664caac521fdb47c3f66a85
Ismael Granillo <igranillo@hawk.iit.edu>
2019-02-19 17:13:16 -0600

    submitting form fname and lname

--- index.php
+++ index.php
@@ -5,6 +5,8 @@
 	<title>ITMD-361 Lab 4 Form Processing Script</title>
 </head>
 <body>
-	
+	<form action="http://bloomingdale.sat.iit.edu/kriedan/lab3formscript.php" method="post"></form>
+	First name: <input type="text" name="fname">
+	Last name: <input type="text" name="lname">
 </body>
 </html>
